8b6d64516c5747dae70eac2426077539b071d0f5
borrik <borrik@live.ru>
2018-01-18 10:58:00 +0200

    class procedure fixed

--- apctk.py
+++ apctk.py
@@ -3,7 +3,7 @@ import time
 import io
 
 class Apconfg():
-    def __init__(self):
+    def __init__(self,mode,command):
         if __name__ == "__main__":
             self.buff = io.BytesIO(b'')
             self.ser = serial.Serial('COM4',
@@ -13,23 +13,30 @@ class Apconfg():
                                      stopbits=1,
                                      parity=serial.PARITY_NONE,
                                      bytesize=8)
-            self.command('olReboot all')
-
-  #sio = io.TextIOWrapper(ser,  newline='\r')
+            if mode == 'verify':
+                pass
+            elif mode == 'exec':
+                self.cmd=command
+                #self.command(command)
+            else:
+                print('invalid mode')
 
     def inwait(self):
         if (self.ser.inWaiting() > 0):
+
             # if incoming bytes are waiting to be read from the serial input buffer
+            self.data_str = self.ser.read(self.ser.inWaiting()).decode('ascii')
             self.buff.write(self.ser.read(self.ser.inWaiting()))
             # read the bytes and convert from binary array to ASCII
-            self.data_str = self.ser.read(self.ser.inWaiting()).decode('ascii')
-            print('data',self.data_str, end='')
+
+            print(self.data_str, end='')
 
     def sendcom(self,cmd=b''):
+        self.inwait()
         self.ser.write(cmd)
         self.ser.write(bytes("\r", encoding='ascii'))
+        time.sleep(1)
         self.inwait()
-
 
     def login(self):
         for e in range(3):
@@ -39,13 +46,12 @@ class Apconfg():
         self.ser.read_until(b"Password")
         self.sendcom(b'apc')
         self.ser.read_until(b"apc>")
-        self.inwait()
 
-    def command(self,cmd='tcpip'):
+    def command(self):
         print('Opening com interface...')
         self.login()
-        self.sendcom(cmd.encode())
+        self.sendcom(self.cmd.encode())
         self.sendcom(b'exit')
-        self.inwait()
 
-app=Apconfg()
+app=Apconfg(mode='exec',command='olReboot all',) #command, mode(exec, verify)
+app.command()
